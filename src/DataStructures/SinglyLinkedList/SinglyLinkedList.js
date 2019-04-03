05295fd6f968203a381f8f7dddcd463eba7ab3be
maddenvvs <maddenvvs@gmail.com>
2019-04-03 10:13:19 +0400

    Added .editorconfig and updated source code

--- src/DataStructures/SinglyLinkedList/SinglyLinkedList.js
+++ src/DataStructures/SinglyLinkedList/SinglyLinkedList.js
@@ -1,207 +1,207 @@
 class SinglyLinkedList {
-  constructor() {
-    this.clear();
-  }
-
-  get length() {
-    return this.count;
-  }
-
-  get count() {
-    return this._count;
-  }
-
-  get isEmpty() {
-    return this.count === 0;
-  }
-
-  append(value) {
-    const nodeToAppend = new SinglyLinkedList.Node(value);
-
-    return this.appendNode(nodeToAppend);
-  }
-
-  appendNode(node) {
-    if (!node) throw new Error("Incorrect node value");
-
-    if (!this._head) {
-      this._head = this._tail = node;
-    } else {
-      this._tail.next = node;
-      this._tail = this._tail.next;
+    constructor() {
+        this.clear();
     }
 
-    this._count++;
-
-    return this;
-  }
-
-  prepend(value) {
-    const nodeToPrepend = new SinglyLinkedList.Node(value);
-
-    return this.prependNode(nodeToPrepend);
-  }
-
-  prependNode(node) {
-    if (!node) throw new Error("Incorrect node value");
-
-    if (!this._head) {
-      this._head = this._tail = node;
-    } else {
-      node.next = this._head;
-      this._head = node;
+    get length() {
+        return this.count;
     }
 
-    this._count++;
-
-    return this;
-  }
-
-  insertAt(value, index) {
-    const nodeToInsert = new SinglyLinkedList.Node(value);
-
-    return this.insertNodeAt(nodeToInsert, index);
-  }
-
-  insertNodeAt(node, index) {
-    if (!node) throw new Error("Incorrect node value");
-
-    if (index === 0) {
-      return this.prependNode(node);
-    } else if (index === this.count) {
-      return this.appendNode(node);
-    } else if (index > 0 && index < this.count) {
-      const nodeBefore = this.getNodeAt(index - 1);
-
-      node.next = nodeBefore.next;
-      nodeBefore.next = node;
-
-      this._count++;
-
-      return this;
+    get count() {
+        return this._count;
     }
 
-    throw new Error("Index out of range");
-  }
-
-  getAt(index) {
-    return this.getNodeAt(index).value;
-  }
-
-  getNodeAt(index) {
-    if (this.isEmpty || index < 0 || index >= this.count)
-      throw new Error("Index out of range");
-
-    if (index === 0) {
-      return this._head;
-    } else if (index === this.count - 1) {
-      return this._tail;
-    } else {
-      let currentNode = this._head;
-      while (index > 0) {
-        currentNode = currentNode.next;
-        index--;
-      }
-
-      return currentNode;
-    }
-  }
-
-  removeFirst() {
-    if (this.isEmpty) throw new Error("Cannot remove from empty list");
-
-    if (this._head === this._tail) {
-      this.clear();
-      return this;
-    } else {
-      this._head = this._head.next;
-      this._count--;
-
-      return this;
-    }
-  }
-
-  removeLast() {
-    if (this.isEmpty) throw new Error("Cannot remove from empty list");
-
-    if (this._head === this._tail) {
-      this.clear();
-      return this;
-    } else {
-      const nodeBefore = this.getNodeAt(this.count - 2);
-
-      this._tail = nodeBefore;
-      this._tail.next = null;
-
-      this._count--;
-
-      return this;
-    }
-  }
-
-  removeAt(index) {
-    if (this.isEmpty || index < 0 || index >= this.count)
-      throw new Error("Index out of range");
-
-    if (index === 0) {
-      return this.removeFirst();
-    } else if (index === this.count - 1) {
-      return this.removeLast();
-    } else {
-      const nodeBefore = this.getNodeAt(index - 1);
-      nodeBefore.next = nodeBefore.next.next;
-
-      this._count--;
-
-      return this;
-    }
-  }
-
-  indexOf(value) {
-    if (value instanceof SinglyLinkedList.Node) {
-      return this.indexOfNode(value);
+    get isEmpty() {
+        return this.count === 0;
     }
 
-    let foundIndex = 0,
-      currentNode = this._head;
+    append(value) {
+        const nodeToAppend = new SinglyLinkedList.Node(value);
 
-    while (currentNode != null && currentNode.value != value) {
-      currentNode = currentNode.next;
-      foundIndex++;
+        return this.appendNode(nodeToAppend);
     }
 
-    return currentNode == null ? -1 : foundIndex;
-  }
+    appendNode(node) {
+        if (!node) throw new Error("Incorrect node value");
 
-  indexOfNode(node) {
-    let foundIndex = 0,
-      currentNode = this._head;
+        if (!this._head) {
+            this._head = this._tail = node;
+        } else {
+            this._tail.next = node;
+            this._tail = this._tail.next;
+        }
 
-    while (currentNode != null && currentNode != node) {
-      currentNode = currentNode.next;
-      foundIndex++;
+        this._count++;
+
+        return this;
     }
 
-    return currentNode == null ? -1 : foundIndex;
-  }
+    prepend(value) {
+        const nodeToPrepend = new SinglyLinkedList.Node(value);
 
-  clear() {
-    this._head = this._tail = null;
-    this._count = 0;
-  }
-
-  *[Symbol.iterator]() {
-    let currentNode = this._head;
-    while (currentNode !== null) {
-      yield currentNode.value;
-      currentNode = currentNode.next;
+        return this.prependNode(nodeToPrepend);
     }
-  }
+
+    prependNode(node) {
+        if (!node) throw new Error("Incorrect node value");
+
+        if (!this._head) {
+            this._head = this._tail = node;
+        } else {
+            node.next = this._head;
+            this._head = node;
+        }
+
+        this._count++;
+
+        return this;
+    }
+
+    insertAt(value, index) {
+        const nodeToInsert = new SinglyLinkedList.Node(value);
+
+        return this.insertNodeAt(nodeToInsert, index);
+    }
+
+    insertNodeAt(node, index) {
+        if (!node) throw new Error("Incorrect node value");
+
+        if (index === 0) {
+            return this.prependNode(node);
+        } else if (index === this.count) {
+            return this.appendNode(node);
+        } else if (index > 0 && index < this.count) {
+            const nodeBefore = this.getNodeAt(index - 1);
+
+            node.next = nodeBefore.next;
+            nodeBefore.next = node;
+
+            this._count++;
+
+            return this;
+        }
+
+        throw new Error("Index out of range");
+    }
+
+    getAt(index) {
+        return this.getNodeAt(index).value;
+    }
+
+    getNodeAt(index) {
+        if (this.isEmpty || index < 0 || index >= this.count)
+            throw new Error("Index out of range");
+
+        if (index === 0) {
+            return this._head;
+        } else if (index === this.count - 1) {
+            return this._tail;
+        } else {
+            let currentNode = this._head;
+            while (index > 0) {
+                currentNode = currentNode.next;
+                index--;
+            }
+
+            return currentNode;
+        }
+    }
+
+    removeFirst() {
+        if (this.isEmpty) throw new Error("Cannot remove from empty list");
+
+        if (this._head === this._tail) {
+            this.clear();
+            return this;
+        } else {
+            this._head = this._head.next;
+            this._count--;
+
+            return this;
+        }
+    }
+
+    removeLast() {
+        if (this.isEmpty) throw new Error("Cannot remove from empty list");
+
+        if (this._head === this._tail) {
+            this.clear();
+            return this;
+        } else {
+            const nodeBefore = this.getNodeAt(this.count - 2);
+
+            this._tail = nodeBefore;
+            this._tail.next = null;
+
+            this._count--;
+
+            return this;
+        }
+    }
+
+    removeAt(index) {
+        if (this.isEmpty || index < 0 || index >= this.count)
+            throw new Error("Index out of range");
+
+        if (index === 0) {
+            return this.removeFirst();
+        } else if (index === this.count - 1) {
+            return this.removeLast();
+        } else {
+            const nodeBefore = this.getNodeAt(index - 1);
+            nodeBefore.next = nodeBefore.next.next;
+
+            this._count--;
+
+            return this;
+        }
+    }
+
+    indexOf(value) {
+        if (value instanceof SinglyLinkedList.Node) {
+            return this.indexOfNode(value);
+        }
+
+        let foundIndex = 0,
+            currentNode = this._head;
+
+        while (currentNode != null && currentNode.value != value) {
+            currentNode = currentNode.next;
+            foundIndex++;
+        }
+
+        return currentNode == null ? -1 : foundIndex;
+    }
+
+    indexOfNode(node) {
+        let foundIndex = 0,
+            currentNode = this._head;
+
+        while (currentNode != null && currentNode != node) {
+            currentNode = currentNode.next;
+            foundIndex++;
+        }
+
+        return currentNode == null ? -1 : foundIndex;
+    }
+
+    clear() {
+        this._head = this._tail = null;
+        this._count = 0;
+    }
+
+    *[Symbol.iterator]() {
+        let currentNode = this._head;
+        while (currentNode !== null) {
+            yield currentNode.value;
+            currentNode = currentNode.next;
+        }
+    }
 }
 
 SinglyLinkedList.Node = class Node {
-  constructor(value) {
-    this.value = value;
-    this.next = null;
-  }
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
 };
--- src/DataStructures/UnionFind/UnionFind.js
+++ src/DataStructures/UnionFind/UnionFind.js
@@ -1,69 +1,69 @@
 class UnionFind {
-  constructor() {
-    this._parent = {};
-    this._size = {};
-    this._disjointSetsCount = 0;
-  }
-
-  get length() {
-    return Object.keys(this._parent).length;
-  }
-
-  get disjointSetsCount() {
-    return this._disjointSetsCount;
-  }
-
-  sizeOf(item) {
-    return this._size[this.find(item)];
-  }
-
-  makeSet(item) {
-    if (this._parent[item]) {
-      throw new Error("Union-find already contains given item.");
+    constructor() {
+        this._parent = {};
+        this._size = {};
+        this._disjointSetsCount = 0;
     }
 
-    this._parent[item] = item;
-    this._size[item] = 1;
-    this._disjointSetsCount++;
-  }
-
-  find(item) {
-    if (!this._parent[item]) {
-      throw new Error("Union-find doesn't contain provided item.");
+    get length() {
+        return Object.keys(this._parent).length;
     }
 
-    return this._findRoot(item);
-  }
-
-  union(first, second) {
-    const firstRoot = this.find(first),
-      secondRoot = this.find(second);
-
-    if (firstRoot === secondRoot) {
-      return false;
+    get disjointSetsCount() {
+        return this._disjointSetsCount;
     }
 
-    const firstSize = this._size[firstRoot],
-      secondSize = this._size[secondRoot];
-
-    if (firstSize < secondSize) {
-      tmp = firstRoot;
-      firstRoot = secondRoot;
-      secondRoot = tmp;
+    sizeOf(item) {
+        return this._size[this.find(item)];
     }
 
-    this._parent[secondRoot] = firstRoot;
-    this._size[firstRoot] += this._size[secondRoot];
-    this._disjointSetsCount--;
+    makeSet(item) {
+        if (this._parent[item]) {
+            throw new Error("Union-find already contains given item.");
+        }
 
-    return true;
-  }
-
-  _findRoot(item) {
-    if (item === this._parent[item]) {
-      return item;
+        this._parent[item] = item;
+        this._size[item] = 1;
+        this._disjointSetsCount++;
     }
 
-    return (this._parent[item] = this._findRoot(this._parent[item]));
-  }
+    find(item) {
+        if (!this._parent[item]) {
+            throw new Error("Union-find doesn't contain provided item.");
+        }
+
+        return this._findRoot(item);
+    }
+
+    union(first, second) {
+        const firstRoot = this.find(first),
+            secondRoot = this.find(second);
+
+        if (firstRoot === secondRoot) {
+            return false;
+        }
+
+        const firstSize = this._size[firstRoot],
+            secondSize = this._size[secondRoot];
+
+        if (firstSize < secondSize) {
+            tmp = firstRoot;
+            firstRoot = secondRoot;
+            secondRoot = tmp;
+        }
+
+        this._parent[secondRoot] = firstRoot;
+        this._size[firstRoot] += this._size[secondRoot];
+        this._disjointSetsCount--;
+
+        return true;
+    }
+
+    _findRoot(item) {
+        if (item === this._parent[item]) {
+            return item;
+        }
+
+        return (this._parent[item] = this._findRoot(this._parent[item]));
+    }
 }
