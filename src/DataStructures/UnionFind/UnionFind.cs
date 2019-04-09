add4d9d8a44816cebaf0da29f19874c4e23c23b7
maddenvvs <maddenvvs@gmail.com>
2019-04-09 17:02:42 +0400

    Added missing comments for union-find (C#)

--- src/DataStructures/UnionFind/UnionFind.cs
+++ src/DataStructures/UnionFind/UnionFind.cs
@@ -3,23 +3,60 @@ namespace DataStructures
     using System;
     using System.Collections.Generic;
 
+    /// <summary>
+    /// Union-Find (Disjoint Set Union) data structure implementation.
+    /// </summary>
+    /// <typeparam name="T">Type of values stored in the UF.</typeparam>
     public class UnionFind<T>
     {
+        /// <summary>
+        /// Creates empty UF.
+        /// </summary>
         public UnionFind()
             : this(EqualityComparer<T>.Default)
         {
         }
 
+        /// <summary>
+        /// Creates empty UF and populates it with values from the collection.
+        ///
+        /// Time complexity: O(N).
+        ///
+        /// Space complexity: O(N).
+        ///
+        /// N - length of the collection.
+        /// </summary>
+        /// <param name="collection">
+        /// The collection to populate values from.
+        /// </param>
         public UnionFind(IEnumerable<T> collection)
             : this(collection, EqualityComparer<T>.Default)
         {
         }
 
+        /// <summary>
+        /// Creates empty UF with custom equality comparer.
+        /// </summary>
+        /// <param name="comparer"> Custom comparer implementation.</param>
         public UnionFind(IEqualityComparer<T> comparer)
             : this(Array.Empty<T>(), comparer)
         {
         }
 
+        /// <summary>
+        /// Creates empty UF with custom equality comparer and populates it
+        /// with values from the collection.
+        ///
+        /// Time complexity: O(N).
+        ///
+        /// Space complexity: O(N).
+        ///
+        /// N - length of the collection.
+        /// </summary>
+        /// <param name="collection">
+        /// The collection to populate values from.
+        /// </param>
+        /// <param name="comparer"> Custom comparer implementation.</param>
         public UnionFind(IEnumerable<T> collection, IEqualityComparer<T> comparer)
         {
             if (collection == null)
@@ -42,8 +79,24 @@ namespace DataStructures
             }
         }
 
+        /// <summary>
+        /// Returns total number of values stored in the UF.
+        ///
+        /// Time complexity: O(1).
+        ///
+        /// Space complexity: O(1).
+        /// </summary>
+        /// <value>The total number of values.</value>
         public int Count { get => this.Parent.Count; }
 
+        /// <summary>
+        /// Returns total number of disjoint sets in the UF.
+        ///
+        /// Time complexity: O(1).
+        ///
+        /// Space complexity: O(1).
+        /// </summary>
+        /// <value>The total number of disjoint sets.</value>
         public int DisjointSetsCount { get; private set; }
 
         private Dictionary<T, T> Parent { get; set; }
@@ -52,8 +105,33 @@ namespace DataStructures
 
         private IEqualityComparer<T> Comparer { get; set; }
 
+        /// <summary>
+        /// Returns the size of the set the given item belongs to.
+        ///
+        /// Time complexity: O(α(N)).
+        ///
+        /// Space complexity: O(α(N)).
+        ///
+        /// N - number of items in the UF. α(N) - inverse Ackermann function.
+        /// </summary>
+        /// <param name="item">The item of the set of question.</param>
+        /// <exception cref="System.ArgumentException">
+        /// Thrown when given item isn't contained in the UF.
+        /// </exception>
+        /// <returns>Returns the size of the set of question.</returns>
         public int SizeOf(T item) => this.Size[this.Find(item)];
 
+        /// <summary>
+        /// Creates new disjoint set with given item.
+        ///
+        /// Time complexity: O(1).
+        ///
+        /// Space complexity: O(1).
+        /// </summary>
+        /// <param name="item">The item (and root) of the new set.</param>
+        /// <exception cref="System.ArgumentException">
+        /// Thrown when given item is already contained in the UF.
+        /// </exception>
         public void MakeSet(T item)
         {
             if (this.Parent.ContainsKey(item))
@@ -66,6 +144,20 @@ namespace DataStructures
             this.DisjointSetsCount++;
         }
 
+        /// <summary>
+        /// Returns root item of the set the given item belongs to.
+        ///
+        /// Time complexity: O(α(N)).
+        ///
+        /// Space complexity: O(α(N)).
+        ///
+        /// N - number of items in the UF. α(N) - inverse Ackermann function.
+        /// </summary>
+        /// <param name="item">The item of the set.</param>
+        /// <exception cref="System.ArgumentException">
+        /// Thrown when given item isn't contained in the UF.
+        /// </exception>
+        /// <returns>Returns the root item of the found set.</returns>
         public T Find(T item)
         {
             if (!this.Parent.ContainsKey(item))
@@ -76,6 +168,18 @@ namespace DataStructures
             return this.GetRoot(item);
         }
 
+        /// <summary>
+        /// Unions two sets two items are belong to respectively.
+        ///
+        /// Time complexity: O(α(N)).
+        ///
+        /// Space complexity: O(α(N)).
+        ///
+        /// N - number of items in the UF. α(N) - inverse Ackermann function.
+        /// </summary>
+        /// <param name="first">The element of the first set.</param>
+        /// <param name="second">The element of the second set.</param>
+        /// <returns>Returns true if items united, false otherwise.</returns>
         public bool Union(T first, T second)
         {
             T firstRoot = this.Find(first), secondRoot = this.Find(second);
