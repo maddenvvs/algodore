fe646f59b9c1c8be2df270d03b3fa979d5ad826a
maddenvvs <maddenvvs@gmail.com>
2019-04-10 16:10:35 +0400

    Updated union-find (Python3) according to pylint code style

--- src/DataStructures/UnionFind/UnionFind.py
+++ src/DataStructures/UnionFind/UnionFind.py
@@ -1,52 +1,108 @@
+"""
+Union-Find (Disjoint Set Union) data structure implementation.
+"""
+
+__all__ = ['UnionFind']
+
+
 class UnionFind:
+    """
+    Union-find data structure.
+    """
+
     def __init__(self):
         self._parent = {}
         self._size = {}
-        self._disjointSetsCount = 0
+        self._disjoint_sets_count = 0
 
     def __len__(self):
         return len(self._parent)
 
     @property
-    def disjointSetsCount(self):
-        return self._disjointSetsCount
+    def disjoint_sets_count(self):
+        """
+        Returns total number of disjoint sets in the UF.
 
-    def sizeOf(self, item):
+        Time complexity: O(1).
+
+        Space complexity: O(1).
+        """
+
+        return self._disjoint_sets_count
+
+    def size_of(self, item):
+        """
+        Returns the size of the set the given item belongs to.
+
+        Time complexity: O(α(N)).
+
+        Space complexity: O(α(N)).
+
+        N - number of items in the UF. α(N) - inverse Ackermann function.
+        """
+
         return self._size[self.find(item)]
 
-    def makeSet(self, item):
+    def make_set(self, item):
+        """
+        Creates new disjoint set with given item.
+
+        Time complexity: O(1).
+
+        Space complexity: O(1).
+        """
+
         if item in self._parent:
             raise ValueError()
 
         self._parent[item] = item
         self._size[item] = 1
-        self._disjointSetsCount += 1
+        self._disjoint_sets_count += 1
 
     def find(self, item):
+        """
+        Returns root item of the set the given item belongs to.
+
+        Time complexity: O(α(N)).
+
+        Space complexity: O(α(N)).
+
+        N - number of items in the UF. α(N) - inverse Ackermann function.
+        """
+
         if item not in self._parent:
             raise ValueError()
 
-        return self._findRoot(item)
+        return self._find_root(item)
 
     def union(self, first, second):
-        firstRoot, secondRoot = self.find(first), self.find(second)
+        """
+        Unions two sets two items are belong to respectively.
 
-        if firstRoot == secondRoot:
+        Time complexity: O(α(N)).
+
+        Space complexity: O(α(N)).
+
+        N - number of items in the UF. α(N) - inverse Ackermann function.
+        """
+        first_root, second_root = self.find(first), self.find(second)
+
+        if first_root == second_root:
             return False
 
-        firstSize, secondSize = self._size[firstRoot], self._size[secondRoot]
-        if firstSize < secondSize:
-            firstRoot, secondRoot = secondRoot, firstRoot
+        first_size, second_size = self._size[first_root], self._size[second_root]
+        if first_size < second_size:
+            first_root, second_root = second_root, first_root
 
-        self._parent[secondRoot] = firstRoot
-        self._size[firstRoot] += self._size[secondRoot]
-        self._disjointSetsCount -= 1
+        self._parent[second_root] = first_root
+        self._size[first_root] += self._size[second_root]
+        self._disjoint_sets_count -= 1
 
         return True
 
-    def _findRoot(self, item):
+    def _find_root(self, item):
         if item == self._parent[item]:
             return item
 
-        self._parent[item] = self._findRoot(self._parent[item])
+        self._parent[item] = self._find_root(self._parent[item])
         return self._parent[item]
